6f713dd4248fd8bd0e878f7492b825eb2757df86
Shubham_Kumar <shubham.kumar1@in2ittech.com>
2024-05-24 17:43:35 +0530

    fierdst

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const div1 =  document.getElementById('div1');
+ div1.addEventListener('click',()=>{
+    const sideBar = document.querySelector('.footer');
+sideBar.style.flexDirection = 'row-reverse';
+
+    
+
+
+ })
